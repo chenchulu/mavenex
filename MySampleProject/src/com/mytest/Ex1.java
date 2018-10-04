f87c19854f03fdf88fc37743f3c172bdd317f5e3
chenchulu <chenchulua@gmail.com>
2018-10-04 18:46:11 +0530

    Cahnges done on Git REpo
    test

--- MySampleProject/src/com/mytest/Ex1.java
+++ MySampleProject/src/com/mytest/Ex1.java
@@ -4,7 +4,7 @@ public class Ex1 {
 
 	public static void main(String[] args) {
 		
-		System.out.print("Welcome to this world!");
+		System.out.print("Welcome to this world! changed in gitrepo");
 
 	}
 
