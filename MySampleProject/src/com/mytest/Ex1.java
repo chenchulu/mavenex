ffdcf40718617bc20f9c45685958df38e6ac8012
chenchulu <chenchulua@gmail.com>
2018-10-06 11:53:31 +0530

    Changed Java file

--- MySampleProject/src/com/mytest/Ex1.java
+++ MySampleProject/src/com/mytest/Ex1.java
@@ -5,7 +5,7 @@ public class Ex1 {
 	public static void main(String[] args) {
 		
 		//Modified content in local - 8.50AM
-		System.out.print("Welcome to this world!");
+		System.out.print("Welcome to this world! 11:52 On 06/Oct \n");
 		
 		for(int cnt=1;cnt<=10;cnt++){
 			System.out.println("Hello... "+cnt);
